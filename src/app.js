4c1dccf706f73d6a655f40b6fe3fed5075381880
andreiacflopes <andreiacflopes@gmail.com>
2020-12-19 17:25:03 +0000

    Added the relevant weather icon from the API

--- src/app.js
+++ src/app.js
@@ -16,13 +16,14 @@ function formatDate(timpestamp) {
 
 
 function displayTemperature (response) {
-   console.log(response.data);
+      
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
@@ -30,13 +31,19 @@ function displayTemperature (response) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-   
-}
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
+     iconElement.setAttribute( "alt", response.data.weather[0].description);
+
+   }
 
 
 
 let apiKey = "3ca350b553445b933c97a8b98083fd15";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric`;
+let city = "Lisbon";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 axios.get(apiUrl).then(displayTemperature);
