4df07ad6f09b4492bfb12f0f1926a07f6c502a2e
andreiacflopes <andreiacflopes@gmail.com>
2020-12-21 14:04:50 +0000

    Imprlemented the unit converter

--- src/app.js
+++ src/app.js
@@ -25,7 +25,9 @@ function displayTemperature (response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    celsiusTemperature = response.data.main.temp;
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
@@ -52,8 +54,35 @@ function displayTemperature (response) {
        search(cityInputElement.value);
    }
 
-search("New York");
+   function displayFahrenheitTemperature(event){
+       event.preventDefault();
+       let temperatureElement = document.querySelector("#temperature");
+       //remove the active class the celsius link
+       celsiusLink.classList.remove("active");
+       fahrenheitLink.classList.add("active");
+       let fahrenheitTemperature = (celsiusTemperature * 9) /5 +32;
+       temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+    }
+
+function displayCelsiusTemperature(event){
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+    let celsiusTemperature = null;
 
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+search("New York");
